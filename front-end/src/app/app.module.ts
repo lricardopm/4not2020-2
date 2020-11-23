28a63196629bb6cc8c26fb349dc3bee801607a34
lricardopm <lricardopm@gmail.com>
2020-11-23 19:43:44 +0000

    (05/11) Desenvolvimento do componente de listagem de dados

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Habilitar formatação de moeda e data em português
+import { registerLocaleData } from '@angular/common';
+import localePt from "@angular/common/locales/pt";
+registerLocaleData(localePt);
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
